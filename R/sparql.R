f6e9661e81936f8dadb2898756043eef32480595
Giulio Benedetti <giuliobene2000@gmail.com>
2025-09-01 21:25:19 +0300

    Add inner sparql query to remove duplicate taxa before processing

--- R/sparql.R
+++ R/sparql.R
@@ -12,47 +12,47 @@ if( Sys.info()[["sysname"]] == "Darwin" ){
 
 rdflib <- import("rdflib")
 
-#' @importFrom dplyr bind_rows
+#' @importFrom dplyr
 .querySPARQL <- function(module, graph = rdflib$Graph()){
-    
+    # Collapse UniRef90 ids into long string
     uniref.ids <- paste0("\t\t\tuniref:", module, collapse = "\n")
-    
+    # Define first part of query
     query_part1 <- "
         PREFIX uniprot: <http://purl.uniprot.org/core/>
         PREFIX uniref: <http://purl.uniprot.org/uniref/>
-
+        # Outer query to get final names
         SELECT ?name
         WHERE {
             SERVICE <https://sparql.uniprot.org/> {
-            {
-                SELECT DISTINCT ?sciName ?rank
+            {   # Inner query to get distinct taxa
+                SELECT DISTINCT ?taxId
                 WHERE {
+                    # List UniRef90 ids
                     VALUES ?unirefId {
         "
-    
-    query_part3 <- "
+    # Define second part of query
+    query_part2 <- "
                     }
+                    # Bind UniRef90 ids to cluster members
                     ?unirefId uniprot:member ?member.
-                    ?member uniprot:organism ?taxIn.
-                    ?taxIn uniprot:scientificName ?sciName; uniprot:rank ?rank.
+                    # Bind cluster members to taxa
+                    ?member uniprot:organism ?taxId.
                 }
             }
-
+            # Bind taxa to scientific names and ranks
+            ?taxId uniprot:scientificName ?sciName; uniprot:rank ?rank.
+            # Process name to prefix__taxon format
             bind(lcase(substr(strafter(str(?rank), 'Rank_'), 1, 1)) as ?prefix)
             bind(concat(?prefix, '__', ?sciName) as ?name)
             }
         }
         "
-    
     # Build query
-    query <- paste0(query_part1, uniref.ids, query_part3)
-    
+    query <- paste0(query_part1, uniref.ids, query_part2)
     # Execute query
     qres <- graph$query(query)
-    
     # Convert name bindings to vector
     tax.vec <- vapply(qres$bindings, function(binding)
         binding[["name"]], character(1))
-    
     return(tax.vec)
 }
