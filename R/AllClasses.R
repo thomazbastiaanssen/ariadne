44865c41b3fbb77d534508a0918d8ee35e6e6e70
thomaz <thomazbastiaanssen@gmail.com>
2025-09-29 15:37:16 +0200

    Initialized MultiFactorDB & LinkMapDB classes to interface with defined databases.

---
+++ R/AllClasses.R
@@ -0,0 +1,111 @@
+#' MultiFactor S7 container class
+#' @name MultiFactorDB
+#' @rdname MultiFactorDB-class
+#' @description
+#' `MultiFactorDB` is an S7 class to interface, organize and manage multiple
+#' sets of factors in a remote database. `MultiFactorDB` behaves like a regular.
+#' `MultiFactor`. Methods for `MultiFactorDB` aim to follow `factor` behaviour.
+#'
+#' @details
+#' The most straightforward way to construct a `MultiFactor` object is as a
+#' named list of named data.frames. The columns of the data.frames indicate the
+#' category of factor in that column.
+#'
+#' A `MultiFactor` object presents itself similar to a `data.frame`, in the
+#' sense that level types can be called as columns and individual data.frame
+#' components can be called as rows.
+#' `MultiFactor` inherits from `list`; Content can be accessed through regular
+#' list methods (e.g., `[`, `[[`).
+#' @slot levels `Named list of character vectors`. Accessed through `levels(x)`
+#' @slot value Optional. `Named list of vectors` of same size as `MultiFactor`
+#'     content, or `TRUE`, if missing.
+#' @slot map `(sparse) Matrix` specifying which elements contain which levels.
+#' @returns a `MultiFactor` object.
+#' @export
+#'
+MultiFactorDB <- S7::new_class(
+    "MultiFactorDB",
+    package = "ariadne",
+    parent = MultiFactor::MultiFactor,
+    properties  = list(
+        repo = S7::class_character,
+        levels = S7::new_property(getter = function(self) lapply(self, levels)),
+        value = S7::class_list,
+        map = S7::new_property(
+            getter = function(self) MultiFactor:::.mapMultiFactor(self, mode = "counts")
+        )
+    ),
+    constructor = function(x) {
+        # check input
+        if(is.data.frame(x)) {x <- MultiFactor::LinkMap(x)}
+        if(S7::S7_inherits(x, MultiFactor::LinkMap)) x <- list(x = x)
+        stopifnot(
+            "x must be a LinkMap or list of LinkMaps. " =
+                all(vapply(x, MultiFactor:::.check_input_df, FALSE))
+        )
+        x <- lapply(x, LinkMapDB)
+        if(is.null(names(x))) names(x) <- paste0("x_", seq_along(x))
+        # extract levels
+        x   <- MultiFactor:::.unify_levels(x)
+        value    <- lapply(x, \(x) x@value)
+
+
+        S7::new_object(
+            .parent = x,
+            repo = "",
+            value   = value
+        )
+    },
+
+)
+
+#' LinkMapDB S7 container class
+#' @name LinkMapDB
+#' @rdname LinkMapDB-class
+#' @description
+#' `LinkMapDB`is an S7 class to interface, organize and manage sets of factors
+#' in a remote database. Methods for `LinkMapDB` aim to follow `factor`
+#' behaviour.
+#'
+#' @slot levels `Named list` of character vectors depicting levels.
+#' @slot value `Character scalar` Repository information.
+#' @slot is_bool `Boolean`, not used, for compatibility. Returns `TRUE`.
+#' @param x a `data.frame` with two named columns that can be coerced to factors
+#' @returns a `LinkMapDB` object.
+#' @examples
+#' LinkMapDB(ariadne:::ChocoPhlAn[[1L]], repo = "ChocoPhlAn")
+#' @seealso [MultiFactorDB()]
+#' @export
+#'
+LinkMapDB <- S7::new_class(
+    "LinkMapDB",
+    package = "ariadne",
+    parent = MultiFactor::LinkMap,
+    properties  = list(
+        levels   = S7::new_property(getter = function(self) lapply(self, levels)),
+        value    = S7::class_character,
+        is_bool  = S7::class_logical
+    ),
+    constructor = function(x, repo = c("ChocoPhlAn","Woltka")) {
+        value <- match.arg(repo)
+        S7::new_object(
+            .parent = MultiFactor::LinkMap(x),
+            value   = value,
+            is_bool = TRUE
+        )
+    }
+)
+
+ChocoPhlAn = list(
+    eggnog2uniref50    = data.frame(eggnog   = factor(), uniref50 = factor()),
+    eggnog2uniref90    = data.frame(eggnog   = factor(), uniref90 = factor()),
+    go2uniref50        = data.frame(go       = factor(), uniref50 = factor()),
+    go2uniref90        = data.frame(go       = factor(), uniref90 = factor()),
+    ko2uniref50        = data.frame(ko       = factor(), uniref50 = factor()),
+    ko2uniref90        = data.frame(ko       = factor(), uniref90 = factor()),
+    level4ec2uniref50  = data.frame(ec       = factor(), uniref50 = factor()),
+    level4ec2uniref90  = data.frame(ec       = factor(), uniref90 = factor()),
+    pfam2uniref50      = data.frame(pfam     = factor(), uniref50 = factor()),
+    pfam2uniref90      = data.frame(pfam     = factor(), uniref90 = factor()),
+    uniref502uniref90  = data.frame(uniref50 = factor(), uniref90 = factor())
+)
---
+++ R/LinkMapDB-methods.R
@@ -0,0 +1,59 @@
+#' @title Make into a LinkMap
+#' @name as.LinkMap.LinkMapDB
+#' @description
+#' Return a LinkMap with the properties advertised in the `LinkMapDB`.
+#' @return a `LinkMap` object
+#' @importFrom MultiFactor as.LinkMap
+#' @examples
+#' # generate a MultiFactorDB
+#' MF.db <- MultiFactorDB(ariadne:::ChocoPhlAn)
+#'
+#' # Extract one underlying LinkMapDB object
+#' LM.db <- MF.db[[5L]]
+#'
+#' LM <- as.LinkMap(LM.db)
+NULL
+
+#' @export
+#'
+S7::`method<-`(MultiFactor::as.LinkMap, LinkMapDB, function(x) {
+
+    outnames <- names(x)
+    x <- .getCache(.urlLinkMapDB(x))
+
+    # Read file content
+    line.content <- readLines(x)
+
+    # Split elements in each line by tab
+    line.content <- strsplit(line.content, "\t", fixed = TRUE)
+    # Extract keys
+    keys <- vapply(line.content, FUN = function(x) x[1], FUN.VALUE = character(1L))
+    # Extract values
+    values <- lapply(line.content, FUN = function(x) x[-1])
+
+    x <- `names<-`(
+        data.frame(
+            id.x = rep(keys, vapply(values, length, 1L)),
+            id.y = unlist(values, recursive = TRUE, use.names = FALSE)
+        ), outnames
+    )
+    MultiFactor::LinkMap(x)
+}
+)
+
+
+#' @param x LinkMapDB
+#' @noRd
+.urlLinkMapDB <- function(x) {
+    switch (
+        x@value,
+        ChocoPhlAn = paste0(
+            "https://zenodo.org/records/17100034/files/map_",
+            names(x)[1L], "_", names(x)[2L], ".txt.gz"
+        ),
+        Woltka     = paste0(
+            "https://ftp.microbio.me/pub/wol-20April2021/function",
+            names(x)[1L], "/", names(x)[2L], ".map.xz"
+        )
+    )
+}
