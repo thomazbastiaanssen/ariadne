19f9c76ed17fdd974c40f58c8cbb5212613ad3ef
thomaz <thomazbastiaanssen@gmail.com>
2025-10-08 20:15:19 +0200

    Finished push for mapModules to use MultiFactor.
    mapModules now supports stratified analysis stragight from curatedMetagenomicData output.
    
    Some work remains:
    - Likely still bugs in there
    - To add tests against rpm.
    - Code gets complicated in some places

--- R/mapModules.R
+++ R/mapModules.R
@@ -10,13 +10,24 @@
 #'
 #' @param map \code{MultiFactor}. Typically produced by `importModules()`.
 #'
-#' @param x \code{matrix}, or object that can be coerced to `matrix`, such as a
-#'     `data.frame`, with features as rows and samples as columns.
+#' @param x \code{matrix}. Feature table that can be coerced to `matrix`, such
+#' as a `data.frame`, with features as rows and samples as columns.
+#'
+#' @param x.type \code{Character scalar}. Type of ID in \code{row.names(x)}.
+#'     (Default: \code{"uniref90"}).
+#'
+#' @param x.stratfied \code{Logical or character scalar}. If \code{x} is NOT a
+#'     stratified table; `FALSE` (Default). Otherwise, if \code{x} is stratified
+#'     by rows, provide the field separator character.
+#'     For example: \code{feature|subtype} requires \code{x.stratified = "|"}.
+#' @param method \code{Character vector}. Define what output parameter should
+#'     be computed. One of \code{c("sum", "count", "coverage", "presence")}.
+#'     (Default: \code{"sum"}).
 #'
 #' @param coverage.threshold \code{Numeric scalar}. Minimum proportion of
 #'     components (i.e., reaction steps) required to be considered present.
 #'     bounded between 0-1. (Default: 0.8)
-#' @param method \code{Character vector}.
+#'
 #' @param verbose \code{Logical scalar}. Should information on execution be
 #'     printed in the console. (Default: \code{TRUE}).
 #'
@@ -40,10 +51,10 @@
 #' )^2
 #'
 #' # Map modules to feature table
-#' mapModules(map, x, method = "coverage")
-#' mapModules(map, x, method = "sum")
-#' mapModules(map, x, method = "count")
-#' mapModules(map, x, method = "presence")
+#' mapModules(map, x, x.type = "ko", method = "coverage")
+#' mapModules(map, x, x.type = "ko", method = "sum")
+#' mapModules(map, x, x.type = "ko", method = "count")
+#' mapModules(map, x, x.type = "ko", method = "presence")
 #'
 NULL
 
@@ -52,7 +63,8 @@ NULL
 #' @importFrom MultiFactor weave
 #' @noRd
 S7::method(mapModules, MultiFactor) <- function(
-        map, x, method = c("sum", "count", "coverage", "presence"),
+        map, x, x.type = "uniref90", x.stratified = FALSE,
+        method = c("sum", "count", "coverage", "presence"),
         verbose = TRUE, coverage.threshold = 0.8
         ) {
 
@@ -63,68 +75,184 @@ S7::method(mapModules, MultiFactor) <- function(
     if(coverage.threshold <= 0L | coverage.threshold > 1L ) {
         stop("'coverage.threshold' should be between 0-1.", call. = FALSE)
     }
-    if(!inherits(x, "matrix")) {
-        x <- as.matrix(x)
+    if(!x.type %in% colnames(map)) {
+        stop("'x.type' must be found in 'map' argument. 'map' contained:\n",
+        {paste(colnames(map), collapse = ", ")}, ".")
+    }
+    if(!isFALSE(x.stratified) && !is.character(x.stratified)) {
+        stop("'x.stratified' should a character or FALSE.")
     }
 
+    # Organise parameters
     method <- match.arg(arg = method, c("sum", "count", "coverage", "presence"))
 
-    c2f_ind <- grep("_complex2", names(map))
-    c2c_ind <- grep("^component2.*_complex$", names(map))
     m2c_ind <- grep("_component$", names(map))
+    c2c_ind <- grep("^component2.*_complex$", names(map))
+    c2f_ind <- grep("_complex2", names(map))
+
+    module_name  <- names(map[[m2c_ind]])[[1L]]
+    complex_name <- names(map[[c2f_ind]])[[1L]]
+    feature_name <- names(map[[c2f_ind]])[[2L]]
+    all_modules  <- levels(map[[m2c_ind]][[1L]])
+
+
+    complex2xtype <- if(feature_name == x.type) { map[[c2f_ind]] } else {
+        weave(map, .by = c(complex_name, x.type))
+        }
+
+    mod2x <- MultiFactor::weave(
+        map, .by = c(module_name, feature_name)
+    )
+    c2c         <- as.matrix(map[[c2c_ind]], terms = c(2L, 1L))
+    map_m2c <- map[[m2c_ind]]
 
     # Check for presence of all complex components
-    complex2x   <- .map_AND_complex(map[[c2f_ind]], x)
-    c2c         <- as.matrix(map[[c2c_ind]], terms = c(2L, 1L))
-    component2x <- Matrix::crossprod(c2c, complex2x, boolArith = TRUE)
+    if(isFALSE(x.stratified)) {
+        complex2x <-  .map_AND_complex_unstrat(x, complex2xtype)
+
+            component2x <- Matrix::crossprod(c2c, complex2x, boolArith = TRUE)
+            # Assess module coverage
+            module_coverage <- .map_coverage(map_m2c, component2x)
+            if( method == "coverage" ) {
+                rownames(module_coverage) <- levels(map_m2c[[1L]])
+                return(module_coverage)
+            }
+            module_present <- module_coverage >= coverage.threshold
+            if( method == "presence" ) {
+                rownames(module_present) <- levels(map_m2c[[1L]])
+                return(module_present)
+            }
+
+            mod2x <- MultiFactor::weave(map, .by = c(x.type, module_name), out.format = "matrix")
+
+            if( method == "sum" ) {
+                sum_table <- Matrix::crossprod(mod2x, x) * module_present
+
+                rownames(sum_table) <- levels(map_m2c[[1L]])
+                return(sum_table)
+            }
+
+            if( method == "count" ) {
+                count_table <- Matrix::crossprod(mod2x, x != 0L) * module_present
+
+                rownames(count_table) <- levels(map_m2c[[1L]])
+                return(count_table)
+            }
+        }
+
+
+
+        if( verbose ){
+            message(NROW(x), " features were mapped to ",
+                    NROW(module_coverage), " modules.")
+        }
+
+    if(is.character(x.stratified)) {
+
+        x.str  <- .x_strat(row.names(x), x.stratified, x.type)
+        x.keep <- x.str[[1L]] %in% levels(complex2xtype[[x.type]])
+        x <- x[ x.keep, ]
+        x.ids     <- factor(x.str[[1L]][x.keep])
+        row.names(x) <- x.ids
+        x.subtype <- factor(x.str[[2L]][x.keep])
+
+
+        complex2xtype[[x.type]] <- factor(
+            complex2xtype[[x.type]], levels = levels(x.ids)
+        )
+        complex2xtype <- complex2xtype[!is.na(complex2xtype[[x.type]]), ]
+
+        map_mat <- as.matrix(
+            complex2xtype, terms = c(2L, 1L)
+            )
+        complex2x <-  lapply(split.data.frame(x, x.subtype),
+                                FUN = .map_AND_complex_strat,
+                                  map_mat = map_mat, x.ids = x.ids)
+
+        component2x <- lapply(
+            X = complex2x,
+            function(x) Matrix::crossprod(c2c, x, boolArith = TRUE)
+            )
+
     # Assess module coverage
-    module_coverage <- .map_coverage(map[[m2c_ind]], component2x)
+    module_coverage <- lapply(component2x, .map_coverage, x = map_m2c)
 
-    if( verbose ){
-        message(NROW(x), " features were mapped to ",
-                NROW(module_coverage), " modules.")
-    }
     if( method == "coverage" ) {
-        rownames(module_coverage) <- levels(map[[m2c_ind]][[1L]])
-        return(module_coverage)
+
+        out <- do.call(rbind, module_coverage)
+        row.names(out) <- paste(
+            rep(all_modules, times = length(names(module_coverage))),
+            rep(names(module_coverage), each = length(all_modules)),
+            sep = "|")
+        return(out)
     }
-    module_present <- module_coverage >= coverage.threshold
+    module_present <- lapply(module_coverage,  `>=`,  coverage.threshold)
     if( method == "presence" ) {
-        rownames(module_present) <- levels(map[[m2c_ind]][[1L]])
-        return(module_present)
+        out <- do.call(rbind, module_present)
+        row.names(out) <- paste(
+            rep(all_modules, times = length(names(module_coverage))),
+            rep(names(module_coverage), each = length(all_modules)),
+            sep = "|")
+        return(out)
     }
 
-    module_name <- names(map[[m2c_ind]])[[1L]]
-    x_name <- names(map[[c2f_ind]])[[2L]]
 
-    mod2x <- MultiFactor::weave(map, .by = c(x_name, module_name), out.format = "matrix")
 
-    if( method == "sum" ) {
-        sum_table <- Matrix::crossprod(mod2x, x) * module_present
+    mod2x <- MultiFactor::weave(map, .by = c(x.type, module_name), out.format = "matrix")
 
-        rownames(sum_table) <- levels(map[[m2c_ind]][[1L]])
-        return(sum_table)
-    }
+    if( method %in% c("sum", "count" )) {
+        res_table <- lapply(
+            split.data.frame(x, x.subtype),
+            .crossprod_strat, mod2x, x.ids, method
+            )
+        res_table <- mapply(`*`, res_table, module_present)
 
-    if( method == "count" ) {
-        count_table <- Matrix::crossprod(mod2x, x != 0L) * module_present
+        out <- do.call(rbind, res_table)
+        row.names(out) <- paste(
+            rep(all_modules, times = length(names(module_coverage))),
+            rep(names(module_coverage), each = length(all_modules)),
+            sep = "|")
+        return(out)
 
-        rownames(count_table) <- levels(map[[m2c_ind]][[1L]])
-        return(count_table)
+
     }
 
 }
+}
 
-# x = module limkmap, with complex on the left (1L), features on the right (2L).
-# y = feature table
+# map = module limkmap, with complex on the left (1L), features on the right (2L).
+# x = feature table
 # return = bool matrix indicating full complex presence, row, per sample, col.
 #
-.map_AND_complex <- function(x, y) {
-    x_mat <- as.matrix(x, terms = c(2L, 1L))
+.map_AND_complex_unstrat <- function(x, map) {
+    map_mat <- as.matrix(map, terms = c(2L, 1L))
     Matrix::Matrix(
-        Matrix::crossprod(x_mat, y!=0L) >= Matrix::colSums(x_mat),
+        Matrix::crossprod(map_mat, x !=0L ) >= Matrix::colSums(map_mat),
         sparse = TRUE
     )
+}
+
+# map_mat = module linkmap, with complex on the left (1L), features on the right (2L).
+# x = feature table
+# x.ids = x.ids
+# return = bool matrix indicating full complex presence, row, per sample, col.
+#
+.map_AND_complex_strat <- function(x, map_mat, x.ids) {
+    map_mat <- map_mat[match(row.names(x), levels(x.ids)),]
+    if( prod(dim(map_mat)) < 2L ) { return(NULL) }
+
+    Matrix::Matrix(
+        Matrix::crossprod(map_mat, x !=0L ) >= Matrix::colSums(map_mat),
+        sparse = TRUE, dimnames = list(colnames(map_mat), colnames(x))
+    )
+}
+
+.crossprod_strat <- function(x, mod2x, x.ids, method) {
+    if(method == "count") x <- x != 0L
+    mod2x <- mod2x[match(row.names(x), levels(x.ids)),]
+    if( prod(dim(mod2x) ) < 2L ) { return(NULL) }
+
+    Matrix::crossprod(mod2x, x)
 }
 
 # x = module linkmap, with module on the left (1L), component on the right (2L).
@@ -138,6 +266,21 @@ S7::method(mapModules, MultiFactor) <- function(
         sparse = TRUE
     )
 }
+
+# x = stratified feature table, with row.names format feature<sep>subtype
+# sep = separator character.
+# cn colnames of returned data.frmae
+# returns a LinkMap-formatted data.frame.
+.x_strat <- function(x, sep, feature_name = "feature") {
+    scan(
+        text = x, what = list(
+            feature_name = character(),
+            subtype = character()
+            ),
+        sep = sep, quiet = TRUE
+    )
+}
+
 
 # Query taxonomies based on uniref ids from UniProt using SPARQL
 # x = Character vector of uniref IDs
@@ -184,3 +327,5 @@ S7::method(mapModules, MultiFactor) <- function(
         binding[["name"]], character(1L))
     return(tax.vec)
 }
+
+
