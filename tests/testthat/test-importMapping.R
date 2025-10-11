1ff501ce1b141662e9e37e050aaa32ae04350161
thomaz <thomazbastiaanssen@gmail.com>
2025-10-12 00:50:04 +0200

    Removed outdated code in tests

--- tests/testthat/test-importMapping.R
+++ tests/testthat/test-importMapping.R
@@ -1,19 +1,19 @@
 test_that("importMapping", {
 
-    expect_error(
-        importMapping("ChocoPhlAn", subset = c("wrong", "uniref90")),
-        "both terms must be found as colnames in 'x'"
-    )
+  expect_error(
+    importMapping("ChocoPhlAn", subset = c("wrong", "uniref90")),
+    "both terms must be found as colnames in 'x'"
+  )
 
   expect_error(
     importMapping("ChocoPhlAn", subset = c("ko", "wrong")),
     "both terms must be found as colnames in 'x'"
   )
 
-    map <- importMapping("ChocoPhlAn", subset = c("ko", "uniref90"),
-                         dry.run = FALSE)
+  map <- importMapping("ChocoPhlAn", subset = c("ko", "uniref90"),
+                       dry.run = FALSE)
 
-    expect_named(map)
+  expect_named(map)
 
 
 })
