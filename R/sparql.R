225d23441e1fd2f85997e276e72801fb997651e3
thomaz <thomazbastiaanssen@gmail.com>
2025-10-16 09:14:34 +0200

    Drop reticulate, explore raw-dogging sparql with httr::GET.

--- R/sparql.R
+++ R/sparql.R
@@ -1,14 +1,48 @@
-# The following functions are from https://github.com/aourednik/SPARQLchunks
-# under the GPL-3 licence.
-# We will temporarily mirror them here, eagerly awaiting their  ongoing CRAN
-# submission.
 
-#' Fetch data from a SPARQL endpoint and store the output in a dataframe
+sparkmap <- function(x, endpoint = "uniprot", prefixes) {
+    known_endpoints <- c("uniprot")
+    endpoint <- known_endpoints[charmatch(endpoint, table = known_endpoints)]
+    endpoint_url <- switch(endpoint,  "uniprot" = "https://sparql.uniprot.org/")
+
+    query <- .composeSPARQL(x, endpoint_url, prefixes)
+    .querySPARQL(query)
+}
+
 #' @param endpoint The SPARQL endpoint (a URL)
+#' @importFrom utils URLencode
+.composeSPARQL <- function(x, endpoint_url, prefixes) {
+    #uniref.ids <- paste0("uniref:", x, collapse = " ")
+
+
+    #TODO translate ariadne IDs to uniref code.
+
+    # Translate ariadne path to SPARQL
+    # (paste rows sequentially)
+
+
+    q <- paste0("
+SELECT ?unirefId ?sciName ?member ?protein ?enzyme
+WHERE {
+    VALUES ?unirefId {
+    ", paste0("uniref:", x, collapse = " "),
+                "
+    } .
+?unirefId up_core:member ?member .
+?member up_core:organism ?taxId .
+?taxId up_core:scientificName ?sciName .
+?protein up_core:representativeFor ?unirefId .
+?protein ( up_core:enzyme | up_core:domain/up_core:enzyme | up_core:component/up_core:enzyme ) ?enzyme .
+}")
+    # Build query
+    query <- paste0(prefixes, q)
+    query <- gsub("\\+", "%2B", utils::URLencode(query, reserved = TRUE))
+    query <- paste0(endpoint_url, "?query=", query)
+    return(query)
+}
+
+
 #' @param query The SPARQL query (character)
-#' @param autoproxy Try to detect a proxy automatically (boolean). Useful on Windows machines behind corporate firewalls
-#' @param auth Authentication Information (httr-authenticate-object)
-#' @return SPARQL query result in data.frame format
+#' @returns a data.frame of desired query.
 #' @examples
 #' endpoint <- "https://lindas.admin.ch/query"
 #' query <- "PREFIX schema: <http://schema.org/>
@@ -16,208 +50,89 @@
 #'   ?sub a schema:DataCatalog .
 #'   ?subtype a schema:DataType .
 #' }"
-#' result_df <- sparql2df(endpoint, query)
-#' @export
-sparql2df <- function(endpoint, query, autoproxy = FALSE, auth = NULL) {
-    proxy_config <- ifelse(
-        autoproxy,
-        autoproxyconfig(endpoint),
-        httr::use_proxy(url = NULL)
-    )
-    acceptype <- "text/csv"
-    outcontent <- get_outcontent(endpoint, query, acceptype, proxy_config, auth)
-    tryCatch(
-        content <- textConnection(outcontent$content),
-        error = function(e) {
-            stop(
-                sprintf(
-                    "There is something wrong with the output content: %s",
-                    outcontent
-                )
-            )
-        }
-    )
-    tryCatch(
-        {
-            df <- utils::read.csv(content)
-        },
-        error = function(e) {
-            # utils::browseURL(outcontent$httpquery)
-            stop(
-                sprintf(
-                    "Reply from SPARQL endpoint received but could not convert it to a data.frame.\nVerify the query result in a web browser:\n%s",
-                    outcontent$httpquery
-                )
-            )
-        }
-    )
-    return(df)
-}
-
-#' Get the content from the endpoint
-#' @param endpoint The SPARQL endpoint (URL)
-#' @param query The SPARQL query (character)
-#' @param acceptype 'text/csv' or 'text/xml' (character)
-#' @param proxy_config Detected proxy configuration (list)
-#' @param auth Authentication Information (httr-authenticate-object)
-#' @return The result of the SPARQL query as a list or, if this fails, failure message.
+#' result_df <- .querySPARQL(endpoint, query)
+#'
+#' @importFrom httr content GET timeout add_headers
+#' @importFrom utils URLencode read.csv
 #' @noRd
-get_outcontent <- function(endpoint, query, acceptype, proxy_config, auth = NULL) {
-    qm <- paste(endpoint, "?", "query", "=",
-                gsub("\\+", "%2B", utils::URLencode(query, reserved = TRUE)), "",
-                sep = ""
+.querySPARQL <- function(query) {
+
+    result <- httr::content(
+        httr::GET(
+            query,
+            httr::timeout(60),
+            httr::add_headers(c(Accept = "text/csv"))
+        ),
+        "text", encoding = "UTF-8"
     )
-    message("Preparing to send query to: ", endpoint)
-    message("SPARQL string:\n", query)
-    message("Query URL:\n", qm )
-    content <- tryCatch(
-        {
-            out <- httr::GET(
-                qm,
-                proxy_config, auth,
-                httr::timeout(60),
-                httr::add_headers(c(Accept = acceptype)),
-                httr::user_agent("R client SPARQLChunks")
-            )
-            if (out$status == 401) {warning(
-                "Authentication required. Provide valid authentication with the auth parameter"
-            )} else {
-                httr::warn_for_status(out)
-            }
-            httr::content(out, "text", encoding = "UTF-8") # Don't use return(...). If you use return(...) inside a block that is being assigned (x <- tryCatch({...})), you're exiting the function, not just returning a value for assignment.
-        },
-        error = function(e) {
-            # @see https://github.com/r-lib/httr/issues/417
-            # The download.file function in base R uses IE settings, including proxy password, when you use download
-            # method wininet which is now the default on windows.
-            if (is_windows()) {
-                tempfile <- file.path(tempdir(), "temp.txt")
-                utils::download.file(qm,
-                                     method = "wininet",
-                                     headers = c(Accept = acceptype),
-                                     tempfile
-                )
-                temp <- paste(readLines(tempfile), collapse = "\n")
-                unlink(tempfile)
-                temp
-            }
-        }
-    )
-    if (is.null(content) || nchar(content) < 1) {
-        warning(
-            sprintf("First query attempt result is empty. Trying without Accept=%s header. The result is not guaranteed to be a list.",
-                    acceptype)
-        )
-        content <- tryCatch(
-            {
-                out <- httr::GET(
-                    qm,
-                    proxy_config, auth,
-                    httr::timeout(60),
-                    httr::user_agent("R client SPARQLChunks")
-                )
-                if (out$status == 401) {warning(
-                    "Authentication required. Provide valid authentication with the auth parameter"
-                )} else {
-                    httr::warn_for_status(out)
-                }
-                httr::content(out, "text", encoding = "UTF-8")
-            },
-            error = function(e) {
-                if (is_windows()) {
-                    tempfile <- file.path(tempdir(), "temp.txt")
-                    utils::download.file(qm, method = "wininet", tempfile)
-                    temp <- paste(readLines(tempfile), collapse = "\n")
-                    unlink(tempfile)
-                    temp
-                }
-            }
-        )
-        if (is.null(content) || nchar(content) < 1) {
-            warning("The query result is still empty")
-        }
-    }
-    if (inherits(content, "response")) {
-        if (httr::status_code(content) >= 400) {
-            stop(sprintf("HTTP error %s: %s", httr::status_code(content), httr::http_status(content)$message))
-        }
-    }
-    return(list(
-        content = content,
-        httpquery = qm
-    ))
-}
 
-#' Try to determine the proxy settings automatically
-#' @param endpoint The SPARQL endpoint (URL)
-#' @return Confirmation of the proxy setting
-#' @noRd
-autoproxyconfig <- function(endpoint) {
-    message("Trying to determine proxy parameters")
-    proxy_url <- tryCatch(
-        {
-            curl::ie_get_proxy_for_url(endpoint)
-        },
-        error = function(e) {
-            message("Automatic proxy detection with curl::curl::ie_get_proxy_for_url() failed.")
-            return(NULL)
-        }
-    )
-    if (!is.null(proxy_url)) {
-        message(paste("Using proxy:", proxy_url))
-    } else {
-        message(paste("No proxy found, nor needed, to access the endpoint", endpoint))
-    }
-    return(httr::use_proxy(url = proxy_url))
-}
-
-#' Verify if platform is Windows
-#' @return TRUE if platform is Windows, FALSE otherwise
-#' @noRd
-is_windows <- function() {
-    .Platform$OS.type == "windows"
+    utils::read.csv(textConnection(result), stringsAsFactors = TRUE)
 }
 
 
 
-# Query taxonomies based on uniref ids from UniProt using SPARQL
-# x = Character vector of uniref IDs
-.querySPARQL <- function(x, endpoint = "https://sparql.uniprot.org/") {
-    # Collapse UniRef90 ids into long string
-    uniref.ids <- paste0("uniref:", x, collapse = " ")
-    # Define first part of query
-    query_part1 <- "
-        PREFIX uniprot: <http://purl.uniprot.org/core/>
-        PREFIX uniref: <http://purl.uniprot.org/uniref/>
-        # Outer query to get final names
-        SELECT ?name
-        WHERE {
-        {
-            {   # Inner query to get distinct taxa
-                SELECT DISTINCT ?taxId
-                WHERE {
-                    # List UniRef90 ids
-                    VALUES ?unirefId {
-        "
-    # Define second part of query
-    query_part2 <- "
-                    }
-                    # Bind UniRef90 ids to cluster members
-                    ?unirefId uniprot:member ?member.
-                    # Bind cluster members to taxa
-                    ?member uniprot:organism ?taxId.
-                }
-            }
-            # Bind taxa to scientific names and ranks
-            ?taxId uniprot:scientificName ?sciName; uniprot:rank ?rank.
-            # Process name to prefix__taxon format
-            BIND(lcase(substr(strafter(str(?rank), 'Rank_'), 1, 1)) as ?prefix)
-            BIND(concat(?prefix, '__', ?sciName) as ?name)
-            }
-        }
-        "
-    # Build query
-    query <- paste0(query_part1, "\t\t\t", uniref.ids, query_part2)
-    # Execute query
-   sparql2df(endpoint, query)
-}
+#
+# prefixes <-
+#
+#     "PREFIX up_core: <http://purl.uniprot.org/core/>
+# PREFIX uniref: <http://purl.uniprot.org/uniref/>
+# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+# "
+#
+# # May not be used
+# data.frame(
+#     name = c("species", "uniref90", "uniref50", "ec"),
+#     code = c("sciName", "unirefId", "unirefId", "ec"),
+#     pred = c("up_core:scientificName", "uniref:unirefId",
+#              "uniref:unirefId", "up_core:enzyme")
+# )
+#
+#
+#
+# # x <- levels(
+# #     ariadne::importMapping(
+# #         ariadne::ariadne("choco", uniref90 ~ ko),
+# #         dry.run = FALSE
+# #         )
+# #     )[["uniref90"]][1:100]
+#
+#
+# x <-
+#     c("UniRef90_A0A010PZR5", "UniRef90_A0A010PZT4", "UniRef90_A0A010PZU0",
+#       "UniRef90_A0A010PZV8", "UniRef90_A0A010PZW7", "UniRef90_A0A010Q006",
+#       "UniRef90_A0A010Q047", "UniRef90_A0A010Q0B1", "UniRef90_A0A010Q0B7",
+#       "UniRef90_A0A010Q0D0", "UniRef90_A0A010Q0G4", "UniRef90_A0A010Q0J0",
+#       "UniRef90_A0A010Q0K3", "UniRef90_A0A010Q0U1", "UniRef90_A0A010Q136",
+#       "UniRef90_A0A010Q165", "UniRef90_A0A010Q1D5", "UniRef90_A0A010Q1G0",
+#       "UniRef90_A0A010Q1I2", "UniRef90_A0A010Q1I9", "UniRef90_A0A010Q1R3",
+#       "UniRef90_A0A010Q1S8", "UniRef90_A0A010Q1W2", "UniRef90_A0A010Q216",
+#       "UniRef90_A0A010Q273", "UniRef90_A0A010Q2D9", "UniRef90_A0A010Q2E2",
+#       "UniRef90_A0A010Q2I5", "UniRef90_A0A010Q2J6", "UniRef90_A0A010Q2N4",
+#       "UniRef90_A0A010Q2R1", "UniRef90_A0A010Q3J8", "UniRef90_A0A010Q3K6",
+#       "UniRef90_A0A010Q3S3", "UniRef90_A0A010Q3U3", "UniRef90_A0A010Q3W2",
+#       "UniRef90_A0A010Q3X9", "UniRef90_A0A010Q454", "UniRef90_A0A010Q484",
+#       "UniRef90_A0A010Q4B9", "UniRef90_A0A010Q4D5", "UniRef90_A0A010Q4J6",
+#       "UniRef90_A0A010Q4L3", "UniRef90_A0A010Q4P4", "UniRef90_A0A010Q4T7",
+#       "UniRef90_A0A010Q519", "UniRef90_A0A010Q536", "UniRef90_A0A010Q554",
+#       "UniRef90_A0A010Q578", "UniRef90_A0A010Q5E7", "UniRef90_A0A010Q5Q2",
+#       "UniRef90_A0A010Q5R3", "UniRef90_A0A010Q5X4", "UniRef90_A0A010Q5X9",
+#       "UniRef90_A0A010Q5Y3", "UniRef90_A0A010Q5Y8", "UniRef90_A0A010Q644",
+#       "UniRef90_A0A010Q688", "UniRef90_A0A010Q6H4", "UniRef90_A0A010Q6L2",
+#       "UniRef90_A0A010Q6L8", "UniRef90_A0A010Q6N7", "UniRef90_A0A010Q6R4",
+#       "UniRef90_A0A010Q6S9", "UniRef90_A0A010Q732", "UniRef90_A0A010Q7B7",
+#       "UniRef90_A0A010Q7D6", "UniRef90_A0A010Q7M8", "UniRef90_A0A010Q805",
+#       "UniRef90_A0A010Q813", "UniRef90_A0A010Q832", "UniRef90_A0A010Q859",
+#       "UniRef90_A0A010Q892", "UniRef90_A0A010Q8B0", "UniRef90_A0A010Q8B7",
+#       "UniRef90_A0A010Q8E9", "UniRef90_A0A010Q8F2", "UniRef90_A0A010Q8F7",
+#       "UniRef90_A0A010Q8G6", "UniRef90_A0A010Q8H5", "UniRef90_A0A010Q8H9",
+#       "UniRef90_A0A010Q8J3", "UniRef90_A0A010Q8N5", "UniRef90_A0A010Q8P2",
+#       "UniRef90_A0A010Q8P3", "UniRef90_A0A010Q8U1", "UniRef90_A0A010Q8Y7",
+#       "UniRef90_A0A010Q8Y9", "UniRef90_A0A010Q8Z8", "UniRef90_A0A010Q910",
+#       "UniRef90_A0A010Q922", "UniRef90_A0A010Q984", "UniRef90_A0A010Q985",
+#       "UniRef90_A0A010Q9A4", "UniRef90_A0A010Q9B7", "UniRef90_A0A010Q9E1",
+#       "UniRef90_A0A010Q9E5", "UniRef90_A0A010Q9E7", "UniRef90_A0A010Q9H0",
+#       "UniRef90_A0A010Q9I7")
+#
+#
+# sparkmap(x, endpoint = "uniprot", prefixes = prefixes)
+#
--- R/zzz.R
+++ R/zzz.R
@@ -1,18 +1,3 @@
 #' @import S7
 #' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
-
-#' @importFrom reticulate py_require import
-.onLoad <- function(libname, pkgname){
-    # Register S7 methods
-    S7::methods_register()
-    # Import Python dependencies
-    reticulate::py_require("rdflib")
-    rdflib <<- reticulate::import("rdflib")
-    # Use unverified ssl for MacOS
-    if( Sys.info()[["sysname"]] == "Darwin" ){
-        reticulate::py_require("ssl")
-        ssl <<- reticulate::import("ssl")
-        ssl$`_create_default_https_context` <- ssl$`_create_unverified_context`
-    }
-}
