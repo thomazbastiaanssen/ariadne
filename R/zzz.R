d50ad9210e514b25b486631caa8eb2cde8757ea9
thomaz <thomazbastiaanssen@gmail.com>
2026-02-03 08:46:37 +0100

    restore S7 method registration

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,11 @@
 #' @import S7
 #' @rawNamespace if (getRversion() < "4.3.0") importFrom("S7", "@")
 NULL
+
+rlang::on_load({
+    S7::methods_register()
+})
+
+.onLoad <- function(...) {
+    rlang::run_on_load()
+}
